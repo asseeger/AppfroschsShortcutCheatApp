d8f67f42797c635f7b394e5a6eb2f96fcb87aee5
Andreas Seeger <splash@appfros.ch>
2020-12-08 18:06:40 +0100

    Initial Commit

---
+++ Shared/AppfroschsShortcutCheatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppfroschsShortcutCheatAppApp.swift
+//  Shared
+//
+//  Created by Andreas Seeger on 08.12.20.
+//
+
+import SwiftUI
+
+@main
+struct AppfroschsShortcutCheatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
